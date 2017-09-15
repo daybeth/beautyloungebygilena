4d6a6740dd8eaaec49c91ee1e3c5408f14e98404
Daybeth Vasquez <daybethvj@hotmail.com>
2017-09-14 22:10:22 -0700

    created partials, navBar and routes

--- client/my_angular/config.js
+++ client/my_angular/config.js
@@ -1,12 +1,37 @@
-var app = angular.module('app', ['ngMaterial','angular-carousel', 'angular.filter','ngRoute', 'ngMessages','angularUtils.directives.dirPagination','angular-loading-bar']);
+var app = angular.module('app', [ 'ngRoute']);
 /* configuration for angular route */
 app.config(function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: '/partials/home.html',
-      controller: 'showAllController'
+      templateUrl: '/partials/about.html',
+      controller: 'controller'
+    })
+     .when('/gallery', {
+      templateUrl: '/partials/gallery.html',
+      controller: 'controller'
+    })
+      .when('/contact', {
+      templateUrl: '/partials/contact.html',
+      controller: 'controller'
+    })
+       .when('/facials', {
+      templateUrl: '/partials/facials.html',
+      controller: 'controller'
+    })
+      .when('/wax', {
+      templateUrl: '/partials/wax.html',
+      controller: 'controller'
+    })
+      .when('/beautyBar', {
+      templateUrl: '/partials/beautyBar.html',
+      controller: 'controller'
+    })
+      .when('/massages', {
+      templateUrl: '/partials/massages.html',
+      controller: 'controller'
     })
     .otherwise({
       redirectTo: '/'
     });
 });
+
--- client/my_angular/controller.js
+++ client/my_angular/controller.js
@@ -1,36 +1,5 @@
-app.controller('chatRoomController',['$scope', '$routeParams','ShowFactory','UserFactory', function($scope,$routeParams,ShowFactory,UserFactory){
-  function currentUser(){
-		UserFactory.currentUser(function(data){
-			$scope.user = data;
-		});
-	}
-	currentUser();
+app.controller('controller',['$scope', '$routeParams','UserFactory', function($scope,$routeParams,UserFactory){
+
   
-  $(document).ready(function() {
-      var socket = io();
 
-      $('#new_message').submit(function(e){
-        e.preventDefault();
-        socket.emit('chat message', {message: $('#message').val(), name: $('#name').val()});
-        $('#message').val('');
-        return false;
-      });
-      socket.on('chat message', function(data){
-        var msg = "<p>"+ data.name + ": " + data.message+".</p>";
-        $('#text_box').append(msg);
-      });
-    });
-
-    function getOneShow(name){
-      console.log(name)
-    ShowFactory.getOneShow(name,function(data){
-      console.log("getOne",data)
-      $scope.show = data;
-      // console.log("one show controller",data);
-      var time = $scope.show.schedule.time.split(":")
-      $scope.show.schedule.time = new Date(1988,6,4,time[0],time[1]).toLocaleTimeString();
-      // $scope.show.summary.
-    })
-  }
-  getOneShow($routeParams.name);
 }])
