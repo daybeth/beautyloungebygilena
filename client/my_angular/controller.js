35c8dc2a615003e20437df309f17a453b61ccdf2
Daybeth Vasquez <daybethvj@hotmail.com>
2017-09-17 21:28:37 -0700

    added carousel

--- client/my_angular/controller.js
+++ client/my_angular/controller.js
@@ -4,5 +4,26 @@ app.controller('controller',['$scope', '$routeParams','UserFactory', function($s
   $scope.isCollapsed = false;
   $scope.isCollapsedHorizontal = false;
 
- 
+  $scope.myInterval = 5000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  $scope.slides = [
+    {
+      image: '/images/images(2).jpg'
+    },
+    {
+      image: '/images/images.jpg'
+    },
+    {
+      image: '/images/images(3).jpg'
+    },
+    {
+      image: '/images/images(4).jpg'
+    },
+    {
+      image: '/images/images(1).jpg'
+    }
+  ];
+  
+
 }])
